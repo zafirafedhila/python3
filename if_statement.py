67d9aa750e9f88ac92adc18e77daaf8f921f888a
z <zafirafedhila@users.noreply.github.com>
2019-08-25 18:40:14 +0700

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,114 @@
+# if kondisi :
+#     blok kode
+# elif kondisi2 :
+#     blok kode
+# else :
+#     blok kode
+
+# ipk = 4
+# if ipk >= 3.5 :
+#     print('Nerd')
+# elif ipk >=3 :
+#     print('Good')
+# elif ipk >=2.5:
+#     print('Try Again')
+# elif ipk >=2 :
+#     print('Lazy')
+# else :
+#     print('Dropout')
+
+# buahMerah = ['apel', 'jambu', 'stoberi', 'ceri', 'naga']
+# buahKuning = ['mangga', 'jeruk', 'pisang', 'nanas']
+
+# buah = input('Masukkan nama buah: ')
+
+# if (buah in buahMerah) :
+#     print("Buah Merah")
+# elif (buah in buahKuning) :
+#     print('Buah Kuning')
+# else :
+#     print("Tidak Merah")
+
+
+
+
+
+pertanyaanRydha =['apakah suka ice cream?']
+
+
+jawaban = input ('apakah suka ice cream? masukkan jawaban anda:')
+if(jawaban in pertanyaanRydha):
+	print("Ya")
+ 
+
+pertanyaanRydha =['apakah suka nasi padang?']
+
+
+jawaban = input ('apakah suka nasi padang? masukkan jawaban anda:')
+if(jawaban in pertanyaanRydha):
+	print("Ya")
+
+
+pertanyaanRydha =['apakah suka gerpek level 10?']
+
+
+jawaban = input ('apakah suka gerpek level 10? masukkan jawaban anda:')
+if(jawaban in pertanyaanRydha):
+	print("Ya")
+
+
+
+
+
+
+
+
+
+
+
+# pertanyaanRydha = 'apakah suka ice cream?', 'apakah suka nasi padang?'
+# print (pertanyaanRydha)
+# jawaban = input ('masukkan jawaban anda:')
+# if(jawaban in pertanyaanRydha):
+# 	print("Ya")
+   
+# else:
+# 	print("Ya")
+
+
+
+
+# jomblo = True
+# status = ("Menikah", "Single")[jomblo]
+# print (status)
+
+# umur = input("berapa umur kamu? ")
+# aku = "bocah" if umur < 10 else "dewasa"
+# print (aku)
+
+
+
+
+# lulus = ("Apakah kamu lulus? [ya/tidak]: ")
+
+# if lulus == "tidak":
+#    print("Kamu harus ikut ujian")
+
+
+
+
+
+# huruf = raw_input("Masukkan sebuah huruf: ")
+ 
+# if (huruf >= 'A'):
+#     if (huruf <= 'Z'):
+#         print ("Ini adalah Huruf Besar")
+#     elif (huruf >= 'a'):
+#         if (huruf <= 'z'):
+#             print ("Ini adalah huruf kecil")
+#         else:
+#             print ("Huruf > z")
+#     else:
+#         print ("Huruf > z tapi < a")
+# else:
+#     print ("Huruf < A)
